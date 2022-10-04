f86a6c60a103af85e52398e516528bb35549ed05
Vytis Mikašauskas <mikasauskas.vytis@outlook.com>
2022-10-05 01:37:18 +0300

    Added xUnit Test project.

---
+++ QAP.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace QAP.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            Assert.Equal(3, 1 + 2);
+        }
+    }
+}
